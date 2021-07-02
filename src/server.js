c42018d9be8c45f3b3b6b06c08bbb93c17f81dfa
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-07-02 21:15:53 +0700

    install module alias

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const plugins = require("./plugins");
 const routers = require("./routes");
-
+const alias = require("module-alias/register")
 const app = express();
 const port = process.env.APP_PORT;
 
