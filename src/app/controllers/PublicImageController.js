31aaa088519f36bc8d8c95d90b380f6f1519bb62
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-07-03 12:08:08 +0700

    WIP: fetch photos from endpoint

--- src/app/controllers/PublicImageController.js
+++ src/app/controllers/PublicImageController.js
@@ -1,9 +1,22 @@
-const axios = require("../../plugins/axios")
-const parser = require("../../plugins/xml2json")
+const axios = require("../../plugins/axios");
+const path = require("path");
+// const fs = require("fs");
 module.exports = class PublicImageController {
-    static async index(req, res) {
-        let x = await axios.get("photos_public.gne")
-        let result = x.data
-        res.send(result);
-    }
-}
+  static async index(req, res) {
+    let request = await axios.get("photos_public.gne");
+    let result = request.data;
+
+    let list = result.items.map((row) => {
+      // const filename = path.basename(row.media.m);
+      let image;
+      axios.get(row.media.m, { responseType: "arraybuffer" }).then((res) => {
+        image = Buffer.from(res.data, "binary").toString("base64");
+        row.media.photo = image;
+
+        console.log(row);
+      });
+      return row;
+    });
+    res.send(list);
+  }
+};
