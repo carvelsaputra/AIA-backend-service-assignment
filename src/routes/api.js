abd581b15857900afdc1c31df262d1cc3e7c653e
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-07-03 16:10:56 +0700

    set prefix for API

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,6 @@
 const PublicImageController = require("../app/controllers/PublicImageController")
 
 module.exports = function(router) {
-    router.get("/public-image", PublicImageController.index);
+    router.get("/api/public-image", PublicImageController.index);
 }
   
