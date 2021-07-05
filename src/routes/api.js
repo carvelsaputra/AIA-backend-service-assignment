cf93f1b4f4a70519e5bee6b367cb9b9860aecb3f
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-07-05 14:49:44 +0700

    test

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,10 @@
 const PublicImageController = require("../app/controllers/PublicImageController")
 
 module.exports = function(router) {
-    router.get("/api/public-image", PublicImageController.index);
+    router.get('/', function(req, res) {
+        res.send("test");
+    })
 
+    router.get("/api/public-image", PublicImageController.index);
 }
   
