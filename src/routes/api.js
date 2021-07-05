3c466e035e8106e619da8a0a340861eda63eda83
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-07-05 15:08:05 +0700

    remove test route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,6 @@
 const PublicImageController = require("../app/controllers/PublicImageController")
 
 module.exports = function(router) {
-    router.get('/', function(req, res) {
-        res.send("test");
-    })
-
     router.get("/api/public-image", PublicImageController.index);
 }
   
